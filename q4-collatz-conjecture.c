60034b0c439ab550265554f36d77db84df844320
Vysakh A V <vysakhav951@gmail.com>
2022-03-08 15:06:37 +0530

    q4 argparse

--- q4-collatz-conjecture.c
+++ q4-collatz-conjecture.c
@@ -2,22 +2,26 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 //collatz conjecture, do n/2 if even, 3n+1 if odd. eventually should reach 1
-int main() {
+int main(int argc, char* argv[]) {
 
-    int n;
-
-    while(1) {
-        printf("Enter starting number: ");
-        scanf("%d", &n);
-
-        if (n < 0) {
-            printf("Only postive integers allowed!\n");
-            continue;
-        }
-        break;
+    if(argc != 2) {
+        printf("exactly one parameter (integer) required\n");
+        return 0;
     }
+
+    char* s = argv[1];
+    for(int i=0; s[i]!='\0'; i++) {
+        if(!isdigit(s[i])) {
+            printf("Please enter valid positive integer\n");
+            return 0;
+        }
+    }
+
+    int n = atoi(s);
+
 
     pid_t child = fork();
     if (child < 0) {
@@ -38,7 +42,7 @@ int main() {
     }
     else {
         wait(0);
-        printf("\nparent done waiting");
+        printf("\nparent done waiting\n");
     }
 
 
